38a68b0973771b8889fdf1c7bc545101ef65ca46
unknown <guillermolopez6988@yahoo.com>
2016-08-07 19:30:29 -0500

    separate game into classes

--- Card.java
+++ Card.java
@@ -1,57 +1,58 @@
- public class Card {
-        private int val;
-        private String suit;
-        private String face;
-        private String DEFAULT_FACE = "N/A";
-      
-        public Card(int val, String suit) {
-            this.val = val;
-            this.suit = suit;
-            face = DEFAULT_FACE;
-        }
+public class Card {
 
-        public Card(String face, String suit) {
-            this.face = face;
-            this.suit = suit;
-            if (face.equals("ace")) {
-                val = 2;
-            }
-            else {
-                val = 10;
-            }
-        }
+    private int val;
+    private String suit;
+    private String face;
+    private String DEFAULT_FACE = "N/A";
 
-        public void setSuit(String suit) {
-           this.suit = suit;
-        }
+    public Card(int val, String suit) {
+        this.val = val;
+        this.suit = suit;
+        face = DEFAULT_FACE;
+    }
 
-        public void setVal(int val) {
-            this.val = val;
+    public Card(String face, String suit) {
+        this.face = face;
+        this.suit = suit;
+        if (face.equals("ace")) {
+            val = 11;
         }
+        else {
+            val = 10;
+        }
+    }
 
-        public String getSuit() {
-            return suit;
-        }
+    public void setSuit(String suit) {
+        this.suit = suit;
+    }
 
-        public int getVal() {
-            return val;
-        }
-        
-        public String getFace() {
-            return face;
-        }
+    public void setVal(int val) {
+        this.val = val;
+    }
 
-        public boolean isFaceCard() {
-            return (!face.equals(DEFAULT_FACE));
+    public String getSuit() {
+        return suit;
+    }
+
+    public int getVal() {
+        return val;
+    }
+
+    public String getFace() {
+        return face;
+    }
+
+    public boolean isFaceCard() {
+        return (!face.equals(DEFAULT_FACE));
+    }
+
+    @Override
+    public String toString() {
+        if (isFaceCard()) {
+            return (face + " of " + suit);
         }
-        
-        @Override
-        public String toString() {
-            if (isFaceCard()) {
-                return (face + " of " + suit);
-            }
-            else {
-                return (val + " of " + suit);
-            }
+        else {
+            return (val + " of " + suit);
         }
+    }
 }
--- Deck.java
+++ Deck.java
@@ -1,36 +1,64 @@
+import java.util.Random;
 public class Deck {
-   
+    
     private int MAX_CARDS = 52;
     private String[] SUITS = { "spades", "diamonds", "hearts", "clovers" };
-    private String[] FACE_CARDS = {"king", "queen", "jack", "ace"};
-    private Card[] Cards;
+    private String[] FACE_CARDS = {"jack", "queen", "king", "ace"};
+    private Card[] deck;
+    private int topOfDeck;
 
     public Deck() {
-        System.out.println("here 1:");
         buildDeck();
+        shuffle();
+        topOfDeck = MAX_CARDS-1;
     }
 
-    public void buildDeck() {
-        System.out.println("here 2");
+    private void buildDeck() {
+        deck = new Card[MAX_CARDS];
+        int cardIndex = 0;
+
         for (int i = 2; i <= 10; i++) {
             for (String s : SUITS) {
                 Card c = new Card(i,s);
-                System.out.println(c);
+                deck[cardIndex++] = c; 
             }
         }
 
         for (String f : FACE_CARDS) {
             for (String s : SUITS) {
                 Card c = new Card(f, s);
-                System.out.println(c);
+                deck[cardIndex++] = c;
             }
         }
     }
 
+    //fisher yates shuffle
     public void shuffle() {
-        
+        Random ran = new Random();
+        for (int i = deck.length-1; i > 0; i--) {
+            int ranIndex = ran.nextInt(i);
+            swap(deck, i, ranIndex);
+        }
     }
 
-    public void drawCard() {
+    private void swap(Card[] arr, int i, int j) {
+        Card temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    public boolean isEmpty() {
+        return (topOfDeck < 0);
+    }
+        
+    public Card drawCard() {
+       if (isEmpty()){
+           return (new Card(99, "dead"));
+       }
+       else {
+           Card c = deck[topOfDeck];
+           topOfDeck--;
+           return c;
+       }
     }
 }
