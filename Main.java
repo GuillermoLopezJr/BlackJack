d6f61735c22d6cdd0ddb4d31befd152d5917b00f
unknown <guillermolopez6988@yahoo.com>
2016-08-16 01:13:06 -0500

    add Main.java

--- Main.java
+++ Main.java
@@ -1,8 +1,9 @@
 import javax.swing.JFrame;
+
 public class Main {
     public static void main(String[] args) {
-        int WIN_HEIGHT = 800;
         int WIN_WIDTH  = 1200;
+        int WIN_HEIGHT = 800;
         JFrame win = new JFrame("Black Jack");
         win.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         win.setSize(WIN_WIDTH, WIN_HEIGHT);
